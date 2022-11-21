ced2676424764081ec71d1df285e1a52e81f6132
PeterPukalik <pukalik.p@gmail.com>
2022-11-21 22:34:05 +0100

    php a js validacia

--- App/Controllers/HousesController.php
+++ App/Controllers/HousesController.php
@@ -19,16 +19,48 @@ class HousesController extends  AControllerBase
     {
         $data = $this->request()->getPost();
         if(isset($data["nazov"])){ //isset ci exizstuje a empty ci ej zadane cosi
-            $post = new Houses();
-            $post->setNazov($data["nazov"]);
-            $post->setPopis($data["popis"]);
-            $post->setCena($data["cena"]);
-            $post->setFoto($data["foto"]);
-            $post->setInfo($data["info"]);
-            $post->save();
-            ?><script>
+
+
+
+            $nazov = $_POST['nazov'];
+            $popis = $_POST['popis'];
+            $foto = $_POST['foto'];
+            $info = $_POST['info'];
+            $cena = $_POST['cena'];
+
+
+
+            if(!preg_match("/^[0-9]$/",$cena)) {
+                echo "<div class='text-danger'>Zadal si neplatnu sumu, musi obsahovat len cisla</div><br>";
+            }
+            elseif(!(strlen($nazov) > 0) && !(strlen($nazov) < 100)){
+                echo "<div class='text-danger'>Nazov musi byt vyplneny</div><br>";
+
+            }
+            elseif(!(strlen($popis) > 0)){
+                echo "<div class='text-danger'>Popis musi byt vyplneny</div><br>";
+
+            }
+            elseif(!(strlen($foto) > 0)){
+                echo "<div class='text-danger'>Musis zadat odkaz alebo cestu k foto</div><br>";
+
+            }
+            elseif(!(strlen($info) < 5000)){
+                echo "<div class='text-danger'>Popis musi byt vyplneny</div><br>";
+            }
+            else {
+                $post = new Houses();
+                $post->setNazov($data["nazov"]);
+                $post->setPopis($data["popis"]);
+                $post->setFoto($data["foto"]);
+                $post->setInfo($data["info"]);
+                $post->setCena($data["cena"]);
+                $post->save();
+                ?><script>
                     window.open("?c=home", "_blank"); // will open new tab on window.onload
-            </script><?php
+                </script><?php
+            }
+
         }
 
         return $this->html(new Houses());
@@ -66,6 +98,7 @@ class HousesController extends  AControllerBase
                 $post->setInfo($data["info"]);
                 $post->save();
                 return $this->redirect("?c=home");
+
             }
         }
 
